240473bed7cb5f76d90feabe92451db48d70288b
protic <gameonplayon71@gmail.com>
2025-12-27 10:51:53 +0000

    feat: Add Wi-Fi configuration and static IP setup for ESP32
    - Updated `.cargo/config.toml` to include environment variables for Wi-Fi SSID, password, and static IP configuration.
    - Modified `Cargo.lock` to include new dependencies: `defmt`, `edge-dhcp`, `edge-nal`, `edge-nal-embassy`, `picoserve`, and `const-sha1`.
    - Enhanced `main.rs` to initialize Wi-Fi and spawn web tasks using the new `lib` module.
    - Created `lib.rs` to define static initialization macro and modules for web and Wi-Fi functionalities.
    - Developed `web.rs` to implement a simple web server using `picoserve` with an HTML response.
    - Implemented `wifi.rs` to manage Wi-Fi connections, including access point configuration and static IP assignment.
    - Added an HTML file (`index.html`) for the web server's response, detailing Rust and ESP32 features.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,76 @@
+// #![no_std]
+// #![no_main]
+// #![deny(
+//     clippy::mem_forget,
+//     reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
+//     holding buffers for the duration of a data transfer."
+// )]
+
+// use bt_hci::controller::ExternalController;
+// use embassy_executor::Spawner;
+// use embassy_time::{Duration, Timer};
+// use esp_hal::clock::CpuClock;
+// use esp_hal::timer::timg::TimerGroup;
+// use esp_radio::ble::controller::BleConnector;
+// use log::info;
+// use trouble_host::prelude::*;
+
+// #[panic_handler]
+// fn panic(_: &core::panic::PanicInfo) -> ! {
+//     loop {}
+// }
+
+// extern crate alloc;
+
+// const CONNECTIONS_MAX: usize = 1;
+// const L2CAP_CHANNELS_MAX: usize = 1;
+
+// // This creates a default app-descriptor required by the esp-idf bootloader.
+// // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+// esp_bootloader_esp_idf::esp_app_desc!();
+
+// #[esp_rtos::main]
+// async fn main(spawner: Spawner) -> ! {
+//     // generator version: 1.0.1
+
+//     esp_println::logger::init_logger_from_env();
+
+//     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+//     let peripherals = esp_hal::init(config);
+
+//     esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
+//     // COEX needs more RAM - so we've added some more
+//     esp_alloc::heap_allocator!(size: 64 * 1024);
+
+//     let timg0 = TimerGroup::new(peripherals.TIMG0);
+//     let sw_interrupt =
+//         esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+//     esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+
+//     info!("Embassy initialized!");
+
+//     let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
+//     let (mut _wifi_controller, _interfaces) =
+//         esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
+//             .expect("Failed to initialize Wi-Fi controller");
+//     // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
+//     let transport = BleConnector::new(&radio_init, peripherals.BT, Default::default()).unwrap();
+//     let ble_controller = ExternalController::<_, 20>::new(transport);
+//     let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
+//         HostResources::new();
+//     let _stack = trouble_host::new(ble_controller, &mut resources);
+
+//     // TODO: Spawn some tasks
+//     let _ = spawner;
+
+//     loop {
+//         info!("Hello world!");
+//         Timer::after(Duration::from_secs(1)).await;
+//     }
+
+//     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
+// }
+
 #![no_std]
 #![no_main]
 #![deny(
@@ -6,14 +79,16 @@
     holding buffers for the duration of a data transfer."
 )]
 
-use bt_hci::controller::ExternalController;
+use defmt::info;
 use embassy_executor::Spawner;
 use embassy_time::{Duration, Timer};
 use esp_hal::clock::CpuClock;
+use esp_hal::rng::Rng;
 use esp_hal::timer::timg::TimerGroup;
-use esp_radio::ble::controller::BleConnector;
-use log::info;
-use trouble_host::prelude::*;
+use esp_println as _;
+use esp_hal::interrupt::software::SoftwareInterruptControl;
+
+use dev_wallet as lib;
 
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
@@ -22,51 +97,46 @@ fn panic(_: &core::panic::PanicInfo) -> ! {
 
 extern crate alloc;
 
-const CONNECTIONS_MAX: usize = 1;
-const L2CAP_CHANNELS_MAX: usize = 1;
-
 // This creates a default app-descriptor required by the esp-idf bootloader.
 // For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[esp_rtos::main]
 async fn main(spawner: Spawner) -> ! {
-    // generator version: 1.0.1
-
-    esp_println::logger::init_logger_from_env();
+    // generator version: 1.0.0
 
     let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
     let peripherals = esp_hal::init(config);
 
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
-    // COEX needs more RAM - so we've added some more
-    esp_alloc::heap_allocator!(size: 64 * 1024);
+    esp_alloc::heap_allocator!(#[unsafe(link_section = ".dram2_uninit")] size: 32768);
 
     let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
+    
+    let software_interrupt = SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
+
+    esp_rtos::start(timg0.timer0, software_interrupt.software_interrupt0);
 
     info!("Embassy initialized!");
 
-    let radio_init = esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller");
-    let (mut _wifi_controller, _interfaces) =
-        esp_radio::wifi::new(&radio_init, peripherals.WIFI, Default::default())
-            .expect("Failed to initialize Wi-Fi controller");
-    // find more examples https://github.com/embassy-rs/trouble/tree/main/examples/esp32
-    let transport = BleConnector::new(&radio_init, peripherals.BT, Default::default()).unwrap();
-    let ble_controller = ExternalController::<_, 20>::new(transport);
-    let mut resources: HostResources<DefaultPacketPool, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX> =
-        HostResources::new();
-    let _stack = trouble_host::new(ble_controller, &mut resources);
+    let radio_init = &*lib::mk_static!(
+        esp_radio::Controller<'static>,
+        esp_radio::init().expect("Failed to initialize Wi-Fi/BLE controller")
+    );
+    let rng = Rng::new();
 
-    // TODO: Spawn some tasks
-    let _ = spawner;
+    let stack = lib::wifi::start_wifi(radio_init, peripherals.WIFI, rng, &spawner).await;
 
-    loop {
-        info!("Hello world!");
-        Timer::after(Duration::from_secs(1)).await;
+    let web_app = lib::web::WebApp::default();
+    for id in 0..lib::web::WEB_TASK_POOL_SIZE {
+        spawner.must_spawn(lib::web::web_task(
+            id,
+            stack,
+            web_app.router,
+            web_app.config,
+        ));
     }
 
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0/examples/src/bin
+    loop {
+        Timer::after(Duration::from_secs(1)).await;
+    }
 }
